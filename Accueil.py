6734c6e484336647e68d0a04920152873afed04a
Blueberry012 <156487524+Blueberry012@users.noreply.github.com>
2024-04-10 22:51:21 +0200

    Add files via upload

--- Accueil.py
+++ Accueil.py
@@ -5,26 +5,24 @@ IS_DARK_THEME = True
 
 st.set_page_config(layout="wide")
 
-st.title('Application SNCF')
+st.title('Application City Fighting')
 
 st.image("image/map.png", width=250, use_column_width=False, clamp=True)
 
-st.header("Comment inciter les utilisateurs à favoriser les moyens de transport ?")
+st.header("Bienvenue sur City Fighting !")
 
-st.subheader("Bienvenue")
+st.subheader("Qu'est-ce que City Fighting ?")
 
-st.write("Notre application dédiée à la SNCF simplifie la prise de décision en fournissant des données précises sur les gares les plus fréquentées, facilitant ainsi une gestion efficace des flux de passagers.")
+st.write("City Fighting est une application web qui vous permet de comparer différentes villes françaises sur une variété d'aspects importants pour vous aider à prendre une décision éclairée pour votre prochain déménagement. Que vous recherchiez un nouvel emploi, un meilleur climat, des activités culturelles dynamiques ou simplement un changement de cadre de vie, City Fighting vous fournit les données nécessaires pour comparer les options qui s'offrent à vous.")
 
-st.write("Grâce à une analyse approfondie du flux ferroviaire, notre application identifie les points d'amélioration clés, permettant à la SNCF de prendre des décisions éclairées pour renforcer l'efficacité opérationnelle du réseau ferroviaire.")
+st.subheader("Pourquoi utiliser City Fighting ?")
 
-st.write("Nous allons répondre à ses questions :")
+st.write("Comparaison facile: Notre interface conviviale vous permet de comparer rapidement les villes sur une multitude de critères, des données économiques aux informations sur le logement en passant par la météo et plus encore.")
 
-st.write("1) Quelles sont les gares les plus fréquentées ?")
+st.write("Données fiables: Nous utilisons des sources de données ouvertes et vérifiées pour vous fournir des informations précises et à jour sur chaque ville.")
 
-st.write("2) Quelles aménagements faudrait-il faire dans les gares ?")
-
-st.write("3) Où faudrait-il améliorer le flux ferroviaire ?")
+st.write("Prise de décision informée: En ayant accès à une gamme variée de données sur chaque ville, vous pouvez prendre une décision éclairée et trouver l'endroit qui correspond le mieux à vos besoins et à vos préférences.")
 
 st.divider()
 
-st.write("N'hésitez pas à explorer notre application pour trouver des pistes d'optimisation les infrastructures et améliorer l'expérience des voyageurs.")
+st.write("N'hésitez pas à parcourir notre application pour trouver la ville qui vous correspond.")
